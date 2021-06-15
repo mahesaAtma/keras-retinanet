414a031fd04c19f995d0a1fdf5a04e6376dd068d
Mahesa Atmawidya N E S <37969344+mahesaAtma@users.noreply.github.com>
2021-06-15 10:27:16 +0700

    Changes train.py [Adagrad->Adam]

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -127,12 +127,7 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
             'regression': losses.smooth_l1(),
             'classification': losses.focal()
         },
-        optimizer=keras.optimizers.Adagrad(
-            learning_rate=lr,
-            initial_accumulator_value=0.1,
-            epsilon=1e-07,
-            name="Adagrad"
-        )
+        optimizer=keras.optimizers.Adam(lr=lr, clipnorm=optimizer_clipnorm)
     )
     return model, training_model, prediction_model
 
