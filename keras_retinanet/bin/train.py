d7da9b5db7305ec1db62bc07b830301ab498bc9c
Mahesa Atmawidya N E S <37969344+mahesaAtma@users.noreply.github.com>
2021-06-19 10:44:01 +0700

    Changes train.py [Adadelta -> Adam]

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -127,9 +127,7 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
             'regression': losses.smooth_l1(),
             'classification': losses.focal()
         },
-        optimizer=keras.optimizers.Adadelta(
-            learning_rate=lr, rho=0.95, epsilon=1e-07, name="Adadelta"
-        )
+        optimizer=keras.optimizers.Adam(lr=lr, clipnorm=optimizer_clipnorm)
     )
     return model, training_model, prediction_model
 
